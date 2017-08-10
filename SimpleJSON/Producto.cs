d722395bf391e948df3ded0a53e7830bd66db04a
EriccGame <30744672+EriccGame@users.noreply.github.com>
2017-08-10 10:48:30 -0600

    Add files via upload

---
+++ SimpleJSON/Producto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleJSON
+{
+    class Producto
+    {
+        private String sNombre;
+        private Int32 iCantidad;
+        private Double dPrecio;
+        private String[] sTamaño;
+
+        public String Nombre
+        {
+            get { return sNombre; }
+            set { sNombre = value; }
+        }
+
+        public Int32 Cantidad
+        {
+            get { return iCantidad; }
+            set { iCantidad = value; }
+        }
+
+        public Double Precio
+        {
+            get { return dPrecio; }
+            set { dPrecio = value; }
+        }
+
+        public String[] Tamaño
+        {
+            get { return sTamaño; }
+            set { sTamaño = value; }
+        }
+    }
+}
---
+++ SimpleJSON/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace SimpleJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producto P = new Producto();
+            P.Nombre = "Tenis";
+            P.Cantidad = 10;
+            P.Precio = 15.5;
+            P.Tamaño = new String[] { "EC", "CH", "M", "G", "EG" };
+
+            String sJson = JsonConvert.SerializeObject(P, Formatting.Indented);
+
+            FileStream FS = new FileStream(@"C:\Example\JsonFile.json", FileMode.OpenOrCreate);
+            StreamWriter SW = new StreamWriter(FS, Encoding.UTF8);
+            SW.Write(sJson);
+            SW.Flush();
+            SW.Close();
+
+            Console.Write(sJson);
+            Console.ReadKey();
+        }
+    }
+}
